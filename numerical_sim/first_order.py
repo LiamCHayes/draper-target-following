6a5b0f9300e7d19e23f45d9df547f3bace1f4134
LiamCHayes <liamcharleshayes@gmail.com>
2025-08-25 11:08:21 -0400

    Building out visualization functionality

--- numerical_sim/first_order.py
+++ numerical_sim/first_order.py
@@ -1,21 +1,24 @@
 """First order CBF implementation for a moving target - numerical simulation"""
 
 import numpy as np
+import matplotlib.pyplot as plt
+from qpsolvers import solve_qp
+from celluloid import Camera
 from tqdm import tqdm
-from utilities import angleToRotationMatrix
+from utilities import angleToRotationMatrix, drawHalfCone
 
 
 class Simulation:
     def __init__(self):
         # Time related data
-        self.T = 100.000
+        self.T = 50.000
         self.dt = 0.001
         self.t = 0.0
         self.time_stamps = []
 
         # Agent state
         self.position = np.array([0.0, 0.0, 0.0])
-        self.veloctiy = np.array([0.0, 0.0, 0.0])
+        self.velocity = np.array([0.0, 0.0, 0.0])
         self.phi = 0.0
         self.theta = 0.0
         self.psi = 0.0
@@ -26,39 +29,143 @@ class Simulation:
         self.FOV = np.pi / 6.0
         self.camera_axis = np.array([1.0, 0.0, 0.0])
 
-        # Agent control input
-        self.velocity_control = None
-        self.angular_velocity_control = None
-
         # Feature(s) state
         self.features = []
 
+        # Things to track
+        self.positions = []  # History of position
+        self.norm_ep = []  # Position error over time
+        self.norm_ev = []  # Velocity error over time
+
+        # Animation
+        self.animation_fig = plt.figure(1)
+        self.animation_axis = plt.axes(projection="3d")
+        self.camera = Camera(self.animation_fig)
+
     def run(self):
-        for _ in tqdm(np.arange(0.0, self.T, self.dt)):
+        for frame in tqdm(range(len(np.arange(0.0, self.T, self.dt)))):
             # Update time step
             self.t += self.dt
             self.time_stamps.append(self.t)
 
             # Compute agent control input
-            self.compute_u_star()
-            self.apply_cbf()
+            self.compute_u_star()  # PD function in numerical sim
+            self.apply_cbf()  # CBF function in numerical sim
 
-            # Update agent state TODO
+            # Update agent state
+            self.position = self.position + self.velocity_control * self.dt
+            self.positions.append(self.position)
+
+            # Save frame for animation
+            if frame % 33 == 0:
+                self.add_frame()
+
+        # Create a dataframe with all tracked things for analysis TODO
 
     def compute_u_star(self):
         """Computes the nominal trajectory before applying the CBF"""
-        # TODO
-        pass
+        # PD controller constants
+        K_p = 20.8  # Position error gain
+        K_d = 13.3  # Velocity error gain
+
+        # Calculate desired trajectory
+        rx = 1.0
+        omega_x = 0.3
+
+        ry = 10.0
+        omega_y = 0.2
+
+        rz = 2.0
+        omega_z = 0.2
+
+        # Calculate desired position and velocity
+        self.p_des = np.array(
+            [
+                rx * np.sin(omega_x * self.t),
+                ry * np.sin(omega_y * self.t),
+                rz * np.sin(omega_z * self.t),
+            ]
+        )
+
+        self.v_des = np.array(
+            [
+                omega_x * rx * np.cos(omega_x * self.t),
+                omega_y * ry * np.cos(omega_y * self.t),
+                omega_z * rz * np.cos(omega_z * self.t),
+            ]
+        )
+
+        # Calculate control input
+        self.velocity_control = (
+            self.v_des
+            - K_p * (self.position - self.p_des)
+            - K_d * (self.velocity - self.v_des)
+        )
+        self.norm_ep.append(self.position - self.p_des)
+        self.norm_ev.append(self.velocity - self.v_des)
 
     def apply_cbf(self):
-        """Solves the CBF QP and applies the result"""
-        # TODO
-        pass
+        """
+        Solves the CBF QP and applies the result
+
+        Uses the library qp_solvers, which needs:
+        - P, the quadratic term matrix. A positive semi-definite matrix and symmetric matrix that defines the curvature of the quadratic objective. Typically the hessian matrix of second derivatives
+        - q, the linear term vector. Corresponds to the linear weights applied to the variables
+        - G and h, inequality constraints for the optimization problem
+        - A and b, equality constraints for the optimization problem
+        - lb and ub, lower and upper bounds for the optimization problem
+
+        In the context of the CBF, how do we define each of these terms that go into qpsolvers.solve_qp?
+        
+        TODO
+        - Write out the first order optimization problem in terms of P, q, G, h, A, b, lb, and ub.
+        - Implement this in the code
+        """
+        # Calculate current direction of camera axis in world frame
+        z = self.R @ self.camera_axis
+
+        # Initialize QP variables
+
+        # Initialize CBF constants
+
+        # Loop through each feature to compute the CBF constraint for each feature
+
+        # Impose additional constraints to improve robustness
+
+        # Define the cost matrix for the QP and solve
+
+        # Update the control input
+
+        # Store relevant metrics to track
 
     def animation(self):
         """Make a video of the drone following the target"""
-        # TODO
-        pass
+        print("Creating animation")
+        animation = self.camera.animate()
+        animation.save("plots/animation.mp4", writer="ffmpeg", fps=30)
+        print("Done")
+
+    def add_frame(self):
+        self.animation_axis.plot(
+            self.position[0], self.position[1], self.position[2], "ob"
+        )
+        self.animation_axis.plot(self.p_des[0], self.p_des[1], self.p_des[2], "og")
+        for f in self.features:
+            self.animation_axis.plot(f[0], f[1], f[2], "xr")
+
+        drawHalfCone(
+            self.animation_axis, self.position, self.FOV, R=self.R, plot_type="circle"
+        )
+
+        self.animation_axis.set_title("Agent FOV")
+        self.animation_axis.set_xlabel("x (m)")
+        self.animation_axis.set_ylabel("y (m)")
+        self.animation_axis.set_zlabel("z (m)")
+        self.animation_axis.set_xlim([-5.0, 10.0])
+        self.animation_axis.set_ylim([-5.0, 5.0])
+        self.animation_axis.set_zlim([-5.0, 5.0])
+
+        self.camera.snap()
 
     def plot(self):
         """Plots important values over time for debugging"""
--- numerical_sim/utilities.py
+++ numerical_sim/utilities.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+# Constants
+DIM = 3
+I = np.eye(DIM)
+
 
 # Rotations utilities
 def xAxisRotation(theta):
@@ -46,3 +50,71 @@ def rotationMatrixToAngle(R):
     pitch = np.arctan2(-R[2, 0], R[2, 2])
 
     return roll, pitch, yaw
+
+
+# Plotting functionalities
+def drawHalfCone(
+    axis, vertex, aperture, R=I.copy(), height=10.0, resolution=0.5, plot_type="circle"
+):
+    # Draw a half circular cone, default is with
+    # axis parallel to the x axis of the plot
+    # Parameters
+    #            axis       : axis on which to draw
+    #            vertex     : vertex of the cone
+    #            aperture   : half the aperture of the cone
+    #            R          : rotation matrix to rotate the cone
+    #            height     : height of the cone
+    #            resolution : distance between line/circles
+    #            plot_type  : type of plot ("line", "circle")
+
+    if plot_type != "line" and plot_type != "circle":
+        print("Unknown plot type, using default type: circle")
+        plot_type = "circle"
+
+    if plot_type == "circle":
+        # Plot the cone as a series of circles
+        circles = []  # List to store circles
+        depth = resolution  # Initialize depth
+        while depth < height:  # Loop from vertex to total height
+            radius = depth * np.tan(aperture)  # Radius of current circle
+            current_circle = []  # List of points of the current circle
+            for theta in np.arange(0, 2 * np.pi + 0.1, 0.1):  # Loop around the circle
+                point = [
+                    depth,
+                    radius * np.cos(theta),
+                    radius * np.sin(theta),
+                ]  # Compute point on circle along x axis
+                point = R @ np.array(
+                    point
+                )  # Rotate point to align with desired orientation
+                point = point + vertex  # Add vertex coordinates
+                current_circle.append(point)  # Add point to circle
+            circles.append(current_circle)  # Add circle to list
+            depth += resolution  # Update depth
+
+        for circle in circles:
+            axis.plot(
+                [point[0] for point in circle],
+                [point[1] for point in circle],
+                [point[2] for point in circle],
+                "b-",
+                alpha=0.2,
+            )
+
+    if plot_type == "line":
+        end_points = []
+        radius = height * np.tan(aperture)
+        for theta in np.arange(0, 2 * np.pi + 0.1, resolution):
+            end_point = [height, radius * np.cos(theta), radius * np.sin(theta)]
+            end_point = R @ np.array(end_point)
+            end_point = end_point + vertex
+            end_points.append(end_point)
+
+        for end_point in end_points:
+            axis.plot(
+                [vertex[0], end_point[0]],
+                [vertex[1], end_point[1]],
+                "b-",
+                zs=[vertex[2], end_point[2]],
+                alpha=0.2,
+            )
